fcf899779b2af56a7d570e64b1014efb9e2ae347
Dimmerworld <Quadcorenerd@gmail.com>
2018-08-11 19:25:26 +1000

    Sample jason uploaded

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,33 @@
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.translate.AmazonTranslate;
-import com.amazonaws.services.translate.AmazonTranslateClient;
 import com.amazonaws.services.translate.AmazonTranslateClientBuilder;
 import com.amazonaws.services.translate.model.TranslateTextRequest;
 import com.amazonaws.services.translate.model.TranslateTextResult;
 
+import java.util.Scanner;
+
 public class Main {
     private static final String REGION = "us-east-1";
     public static void main(String args[]){
-        // Create credentials using a provider chain. For more information, see
-        // https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html
         AWSCredentialsProvider awsCreds = DefaultAWSCredentialsProviderChain.getInstance();
 
         AmazonTranslate translate = AmazonTranslateClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds.getCredentials()))
                 .withRegion(REGION)
                 .build();
+Scanner in = new Scanner(System.in);
 
+while(true) {
+    System.out.print("Enter text: ");
 
+    TranslateTextRequest request = new TranslateTextRequest()
+            .withText(in.nextLine())
+            .withSourceLanguageCode("en")
+            .withTargetLanguageCode("zh");
+    TranslateTextResult result = translate.translateText(request);
+    System.out.println(result.getTranslatedText());
 
-        TranslateTextRequest request = new TranslateTextRequest()
-                .withText("Hello, world")
-                .withSourceLanguageCode("en")
-                .withTargetLanguageCode("es");
-        TranslateTextResult result  = translate.translateText(request);
-        System.out.println(result.getTranslatedText());
-    }
+}}
 }
