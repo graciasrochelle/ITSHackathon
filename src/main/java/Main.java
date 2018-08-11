0e51212ea1121709c3747a9fad46550802da8690
graciasrochelle <graciasrochelle@gmail.com>
2018-08-12 06:52:23 +1000

    ping db

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,19 +9,22 @@ public class Main {
          AWSCredentialsProvider awsCreds = DefaultAWSCredentialsProviderChain.getInstance();
          Translate tl = new Translate(awsCreds);
          Dynamo db = new Dynamo();
-         FileReader myRunnable = new FileReader(db);
+//         FileReader myRunnable = new FileReader(db);
+//         Thread t = new Thread(myRunnable);
+//         t.start();
+         PingDynamoDB myRunnable = new PingDynamoDB(db);
          Thread t = new Thread(myRunnable);
          t.start();
 //       System.out.println(tl.translateList(db.load()));
-        ArrayList<String> test = new ArrayList<>();
-        test.add("1534011351456");
-        test.add("我该如何解决这个问题");
-        test.add("zh");
-        test.add("EN");
-        test.add(String.valueOf(true));
-        test.add(String.valueOf(true));
+//        ArrayList<String> test = new ArrayList<>();
+//        test.add("1534011351456");
+//        test.add("我该如何解决这个问题");
+//        test.add("zh");
+//        test.add("EN");
+//        test.add(String.valueOf(true));
+//        test.add(String.valueOf(true));
 
-        db.update(test);
+       // db.update(test);
         //System.out.println(tl.translateMessage("Test","en","es",false));
     }
 }
