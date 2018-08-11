f220aa63590f4a6067fd1bfbc5adf17a1f66f931
Dimmerworld <Quadcorenerd@gmail.com>
2018-08-11 21:07:59 +1000

    Translate class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,14 @@
 import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.services.translate.AmazonTranslate;
-import com.amazonaws.services.translate.AmazonTranslateClientBuilder;
-import com.amazonaws.services.translate.model.TranslateTextRequest;
-import com.amazonaws.services. translate.model.TranslateTextResult;
-
-import java.util.Scanner;
 
 public class Main {
-    private static final String REGION = "us-east-1";
+
     public static void main(String args[]){
         AWSCredentialsProvider awsCreds = DefaultAWSCredentialsProviderChain.getInstance();
+        Translate tl = new Translate(awsCreds);
 
-        AmazonTranslate translate = AmazonTranslateClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds.getCredentials()))
-                .withRegion(REGION)
-                .build();
-Scanner in = new Scanner(System.in);
 
-while(true) {
-    System.out.print("Enter text: ");
+        System.out.println(tl.translateMessage("Test","en","zh",false));
 
-    TranslateTextRequest request = new TranslateTextRequest()
-            .withText(in.nextLine())
-            .withSourceLanguageCode("en")
-            .withTargetLanguageCode("zh");
-    TranslateTextResult result = translate.translateText(request);
-    System.out.println(result.getTranslatedText());
-
-}}
+    }
 }
---
+++ src/main/java/Translate.java
@@ -0,0 +1,35 @@
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.services.translate.AmazonTranslate;
+import com.amazonaws.services.translate.AmazonTranslateClientBuilder;
+import com.amazonaws.services.translate.model.TranslateTextRequest;
+import com.amazonaws.services.translate.model.TranslateTextResult;
+
+public class Translate {
+    private static final String REGION = "us-east-1";
+    AmazonTranslate translate;
+
+    Translate(AWSCredentialsProvider creds){
+        translate = AmazonTranslateClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(creds.getCredentials()))
+                .withRegion(REGION)
+                .build();
+    }
+
+   public String translateMessage(String msg, String sLang, String dLang, boolean hasTranslated){
+
+        if(hasTranslated){
+            String temp = dLang;
+            dLang = sLang;
+            sLang = temp;
+        }
+
+       TranslateTextRequest request = new TranslateTextRequest()
+               .withText(msg)
+               .withSourceLanguageCode(sLang)
+               .withTargetLanguageCode(dLang);
+       TranslateTextResult result = translate.translateText(request);
+
+       return result.getTranslatedText();
+   }
+}
